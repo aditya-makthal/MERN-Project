08f94ca1a546c8390c0d6b68ca348a83a9871b62
M Chandra Aditya <87606335+aditya-makthal@users.noreply.github.com>
2023-07-19 23:11:25 +0530

    Late night update

--- backend/server.js
+++ backend/server.js
@@ -12,17 +12,17 @@ app.use(cors({
 }))
 mongoose.connect("mongodb+srv://projectuser:projectpassword@cluster0.qx2ctcj.mongodb.net/CollegeDetails")
 
-app.get('/',(req,res)=>{
-  res.join("Default route invoked")
-})
+app.get('/', (req, res) => {
+  res.send("Default route invoked");
+});
 
-app.get('/getStudents/:option/:value', (req, res)=>{
+app.get('/getStudents/:option/:value', (req, res) => {
   const opt = req.params.option;
   const value = req.params.value;
-  StudentModel.find({[opt]:value})
-  .then(students => res.json(students))
-  .catch(err => res.json(err))
-})
+  StudentModel.find({ [opt]: value })
+    .then(students => res.json(students))
+    .catch(err => res.status(500).json({ error: "An error occurred while fetching students." }));
+});
 
 app.listen(3001, ()=>{
   console.log("Server is running on port 3001");
